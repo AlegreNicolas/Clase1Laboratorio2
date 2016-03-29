70688df2f149318563aafbc06b6d59e195a09fc6
AlegreNicolas <niicolas.alegre@gmail.com>
2016-03-29 09:49:58 -0300

    Uso de objetos (atributo de instancia y metodo de instancia)

---
+++ ProgramacionClase3/ProgramacionClase3/Alumno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionClase3
+{
+    class Alumno
+    {
+        public string nombre;
+
+        public void mostrar()
+        {
+            Console.Write(this.nombre+legajo);//sin el this se puede usar igual 
+            
+
+        }
+        public int legajo;
+
+        public bool compararme(Alumno alumno)//va a devolver true
+        {
+            bool respuesta = false;
+            if (this.nombre == alumno.nombre && this.legajo == alumno.legajo)
+            {
+                respuesta = true;
+            }
+            
+            
+            return respuesta;
+        }
+
+    }
+}
---
+++ ProgramacionClase3/ProgramacionClase3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionClase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            numero = 0;
+            Alumno unalumno;
+            unalumno=new Alumno();
+            //Alumno nuevo = new Alumno();
+            unalumno.nombre = "Nicolas";
+           
+            //Console.WriteLine(unalumno.nombre);
+            unalumno.mostrar();
+            Console.ReadKey();
+
+        }
+    }
+}
